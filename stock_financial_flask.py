799ea2b82484ccd39a7243f094aa2fe0bbac26c4
Mateus <98242839+mateussfeir@users.noreply.github.com>
2023-03-14 00:30:30 -0400

    Update stock_financial_flask.py

--- stock_financial_flask.py
+++ stock_financial_flask.py
@@ -10,28 +10,37 @@ Flask course from Udemy.
 
 Data Formats: HTML and JSON
 
+Goal of this code:
+Make a webpage where the user can choose the company and receive the data related to its financial statement
+(Specially the revenue and profit)
 '''
 
 from flask import Flask
 import requests
+import pandas as pd
 
 app = Flask(__name__)
-my_list = ['1', '2']
 
 @app.route('/')
 def test():
-    return 'This works'
+    return 'Welcome Here you can receive informatoins about TSLA'
 
-@app.route('/getticker', methods=['GET'])
+@app.route('/price', methods=['GET'])
 
 def get_API():
     url = 'https://www.alphavantage.co/query'
-    params = {'function': 'INCOME_STATEMENT',
+    params = {'function': 'TIME_SERIES_DAILY_ADJUSTED', 
             'symbol': 'TSLA',
-            'apikey': 'TE1E1KD330UYLRHQ'}
+            'apikey': 'TE1E1KD330UYLRHQ'}  
     response = requests.get(url, params=params)
     data = response.json()
-    return data
+    df = pd.DataFrame.from_dict(data['Time Series (Daily)'], orient='index')
+    df = df.astype(float)
+    df.index = pd.to_datetime(df.index)
+    new_price = df.iloc[0]['4. close']
+    return 'Price: ' + str(new_price)
+    
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080)
+
